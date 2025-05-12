3fc3c89b9c2aaa5e37124cf8da8c3306b43f12c4
SpikyDNB <natanielbarsky@gmail.com>
2025-05-12 14:07:58 +0300

    fml

---
+++ book shelf/bookshelf.js
@@ -0,0 +1,57 @@
+import { getRootCssStyles } from './cssUtils.js';
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomChoice(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+let spines = Object.values(document.getElementsByClassName("spine"));
+let covers = Object.values(document.getElementsByClassName("cover"));
+let tops = Object.values(document.getElementsByClassName("top"));
+
+let availablePatterns = getRootCssStyles();
+
+let availableColors = [
+    "maroon",
+    "darkgreen",
+    "darkolivegreen",
+    "brown",
+    "saddlebrown",
+    "sienna",
+    "midnightblue",
+];
+
+// assign a random height, pattern and colour to each book
+spines.map(function (s, i) {
+    let randomHeight = getRandomInt(220, 290);
+    s.style.height = `${randomHeight}px`;
+    s.style.top = `${280 - randomHeight}px`;
+
+    let randomPattern = randomChoice(availablePatterns);
+    s.style.backgroundImage = `var(${randomPattern})`;
+
+    let randomColor = randomChoice(availableColors);
+    s.style.backgroundColor = randomColor;
+
+    covers[i].style.height = `${randomHeight}px`;
+    covers[i].style.top = `${280 - randomHeight}px`;
+
+    tops[i].style.top = `${280 - randomHeight}px`;
+});
+//
+// lazy load the book covers on hover
+let books = Object.values(document.getElementsByClassName("book"));
+books.map(function (b, i) {
+    b.onmouseover = function () {
+        let covers = b.getElementsByClassName("cover");
+        Array.from(covers).map(function (c, i) {
+            c.style.backgroundImage = "url(" + c.getAttribute("img") + ")";
+        }
+        )
+    };
+});
